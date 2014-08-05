da992f41f4bb006d7b41cc6aae8a4a342c93e91d
juanlb <juanlb@gmail.com>
2014-08-05 10:03:10 -0300

    Rollback de test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import sys
 
 # Crear el controlador para la impresora fiscal:
 if False or '--epson' in sys.argv:
-    from epsonPrinter import EpsonPrinter
+    from epsonFiscal import EpsonPrinter
     print "Usando driver de Epson"
     model = ["tickeadoras", "epsonlx300+", "tm-220-af"][1]
     printer = EpsonPrinter(deviceFile="COM2", model=model, dummy=False)
@@ -12,54 +12,30 @@ else:
     from hasarPrinter import HasarPrinter
     print "Usando driver de Hasar"
     model = ["615", "715v1", "715v2", "320"][0]
-    printer = HasarPrinter(deviceFile="/dev/ttyUSB0", model=320, dummy=False)
+    printer = HasarPrinter(deviceFile="COM2", model=model, dummy=False)
 
 # Obtener el último número de factura emitida
-#number = printer.getPrinterId() #+ 1
-if False:
-    number = printer.getLastNumber("B") + 1
-    print "datos ", number
-    #print printer.cancelAnyDocument()
-    #print printer.getErrors()
-    print printer.getPrinterId()
+number = printer.getLastNumber("B") + 1
+print "imprimiendo la FC ", number
 
 # Abrir un comprobante fiscal:
-#if model in ("epsonlx300+", ):
-#    # TODO: ajustar en openTicket
-#    printer.openBillTicket("B", "Nombre y Apellido", "Direccion", "0", # nro_doc
-#                           printer.DOC_TYPE_SIN_CALIFICADOR, 
-#                           printer.IVA_TYPE_CONSUMIDOR_FINAL)
-#else:
-#    printer.openTicket()
-
-#print printer.getWarnings('A')
-
-if False:
-    #Cancelar
-    print printer.cancelAnyDocument()
-    #print printer.getErrors()
-    #print printer.getSubtotal()
-else:
+if model in ("epsonlx300+", ):
+    # TODO: ajustar en openTicket
     printer.openBillTicket("B", "Nombre y Apellido", "Direccion", "0", # nro_doc
-                               printer.DOC_TYPE_SIN_CALIFICADOR, 
-                               printer.IVA_TYPE_CONSUMIDOR_FINAL)
+                           printer.DOC_TYPE_SIN_CALIFICADOR, 
+                           printer.IVA_TYPE_CONSUMIDOR_FINAL)
+else:
+    printer.openTicket()
 
-    #printer.openTicket()
+# Facturar Caramelos a $ 1,50, con 21% de IVA, 2 paquetes de cigarrillos a $ 10
+printer.addItem("CARAMELOS", 1, 1.50, 21.0, discount=0, discountDescription="")
+printer.addItem("CIGARRILLOS", 2, 10, 21.0, discount=0, discountDescription="")
 
-    #printer.printNonFiscalText("Si anda me emplumo")
-    #printer.setHeader(["Test de Header"])
+# Pago en efectivo. Si el importe fuera mayor la impresora va a
+# calcular el vuelto
+printer.addPayment("Efectivo", 11.50)
 
-    # Facturar Caramelos a $ 1,50, con 21% de IVA, 2 paquetes de cigarrillos a $ 10
-    printer.addItem("CIGARRILLOS", 2, 1.0, 21.0,  discount=0, discountDescription="")
-    #printer.addItem("CARAMELOS", 1, 1.50, 21.0, discount=0, discountDescription="")
-    printer.addItem("CARAMELOS", 1, 'asdf', 21.0, discount=0, discountDescription="")
-    #print printer.getSubtotal()
-    
+# Cerrar el comprobante fiscal
+printer.closeDocument()
 
-    # Pago en efectivo. Si el importe fuera mayor la impresora va a
-    # calcular el vuelto
-    # printer.addPayment("Efectivo", 11.50)
-
-    # Cerrar el comprobante fiscal
-    # printer.closeDocument()
 
